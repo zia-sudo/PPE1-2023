be738090502e0b6fa8e009081e782174dc8cf2f2
zia <zhi-ya.ma@sorbonne-nouvelle.fr>
2023-10-22 22:38:39 +0200

    vérif arguments

--- Exercice/Fichiers/exo1_2_v.sh
+++ Exercice/Fichiers/exo1_2_v.sh
@@ -9,6 +9,6 @@ fi
 type_entite="$1"
 
 for annee in 2016 2017 2018; do
-  echo "$type_entite pour l'année $annee :"
-  ./exo1_1.sh "$annee" "$type_entite" >> r_exo1.txt
+  echo "Les résultats de l'année "$annee" sont enregistrés dans le fichier r_exo1_v : "
+  ./exo1_1.sh "$annee" "$type_entite" >> r_exo1_v.txt
 done
