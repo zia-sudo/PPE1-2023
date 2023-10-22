f738be5583ec1abd1b7c313e4ef7b53f31a238e1
zia <zhi-ya.ma@sorbonne-nouvelle.fr>
2023-10-22 22:37:56 +0200

    version améliorée

--- Exercice/Fichiers/exo1_2.sh
+++ Exercice/Fichiers/exo1_2.sh
@@ -3,6 +3,6 @@
 type_entite="$1"
 
 for annee in 2016 2017 2018; do
-  echo "$type_entite pour l'année $annee :"
+  echo "Les résultats de l'année "$annee" sont enregistrés dans le fichier r_exo1.txt!"
   ./exo1_1.sh "$annee" "$type_entite" >> r_exo1.txt
 done
